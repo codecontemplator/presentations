05553b0b5b396521b49f244f8519b8205bcf77c7
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2018-10-12 09:53:21 +0200

    .

---
+++ powershell/code/tips and tricks - jump-location.ps1
@@ -0,0 +1,10 @@
+﻿
+# Navigate to most commonly used folder named 'myfolder'
+Jump-Location myfolder
+
+# Navigate to a subfolder named 'subfolder' of 'myfolder'
+Jump-Location myfolder subfolder
+
+# Using alias for Jump-Location 
+j myfolder subfolder
+					

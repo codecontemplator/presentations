5ae9d8962058601f8dea92c00e4ad34e1319ecb5
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2023-08-05 13:01:22 +0200

    Trivial 03 added

---
+++ constraint-programming/trivial/trivial-03/Program.cs
@@ -0,0 +1,32 @@
+﻿using Google.OrTools.Sat;
+
+// Creates the model.
+CpModel model = new CpModel();
+
+// Creates the variables.
+int num_vals = 3;
+
+IntVar x = model.NewIntVar(0, num_vals - 1, "x");
+IntVar y = model.NewIntVar(0, num_vals - 1, "y");
+IntVar z = model.NewIntVar(0, num_vals - 1, "z");
+
+// Creates the constraints.
+model.Add(x != y);
+
+// Optmization
+model.Maximize(x + y + z);
+
+// Creates a solver and solves the model.
+CpSolver solver = new CpSolver();
+CpSolverStatus status = solver.Solve(model);
+
+if (status == CpSolverStatus.Optimal || status == CpSolverStatus.Feasible)
+{
+    Console.WriteLine("x = " + solver.Value(x));
+    Console.WriteLine("y = " + solver.Value(y));
+    Console.WriteLine("z = " + solver.Value(z));
+}
+else
+{
+    Console.WriteLine("No solution found.");
+}

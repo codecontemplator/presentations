ac7e36d7c1407081c6a742da96bcdbb0b9251449
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2023-08-05 13:35:00 +0200

    trival without cp added

---
+++ constraint-programming/trivial/trivial-04/Program.cs
@@ -0,0 +1,30 @@
+﻿Solution solution = null;
+
+for(var x=0; x<=2; x++)
+{
+    for(var y=0; y<=2; y++)
+    {
+        for(var z =0; z<=2; z++)
+        {
+            if (x != y)
+            {
+                var objective = x + y + z;
+                if (solution == null || objective > solution.objective)
+                {
+                    solution = new Solution(x, y, z, objective);
+                }
+            }
+        }
+    }
+}
+
+if (solution == null)
+{
+    Console.WriteLine("No solution found");
+}
+else
+{
+    Console.WriteLine("Solution: " + solution);
+}
+
+record Solution(int x, int y, int z, int objective);

433f650a9ef419d5a103f414df22b2e67a9f600c
daniel.brannstrom <daniel.brannstrom.ext@pengvin.se>
2023-10-24 16:33:17 +0200

    cleanup

--- constraint-programming/python/binpacking.py
+++ constraint-programming/python/binpacking.py
@@ -7,7 +7,7 @@ class Truck(NamedTuple):
 class Box(NamedTuple):
     weight: int
 
-trucks = [Truck(3000), Truck(5000), Truck(8000)]
+trucks = [Truck(3000), Truck(5000), Truck(3000), Truck(3000), Truck(8000)]
 boxes = [Box(3000), Box(3000), Box(4000), Box(2000)]
 
 model = cp_model.CpModel()
@@ -15,7 +15,7 @@ model = cp_model.CpModel()
 boxInTruck = {}
 for ti in range(len(trucks)):
     for bi in range(len(boxes)):
-        boxInTruck[(ti,bi)] = model.NewBoolVar(f"boxInTruck: ti {ti}, bi {bi}")
+        boxInTruck[(bi,ti)] = model.NewBoolVar(f"boxInTruck: ti {ti}, bi {bi}")
 
 truckInUse = []
 for ti in range(len(trucks)):
@@ -23,23 +23,25 @@ for ti in range(len(trucks)):
 
 # a box must be in exactly one truck
 for bi in range(len(boxes)):
-    model.AddExactlyOne([ boxInTruck[(ti,bi)] for ti in range(len(trucks)) ])
+    model.AddExactlyOne([ boxInTruck[(bi,ti)] for ti in range(len(trucks)) ])
 
 # the box weight must not exceed capacity
 for ti in range(len(trucks)):
-    boxWeight = [ boxInTruck[(ti,bi)] * boxes[bi].weight for bi in range(len(boxes)) ]
-    totalWeight = sum(boxWeight)
-    model.Add(totalWeight <= trucks[ti].capacity * truckInUse[ti])
+    weights = [ boxInTruck[(bi,ti)] * boxes[bi].weight for bi in range(len(boxes)) ]
+    model.Add(sum(weights) <= trucks[ti].capacity * truckInUse[ti])
 
 model.Minimize(sum(truckInUse))
 
 solver = cp_model.CpSolver()
 status = solver.Solve(model)
 
-if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-    print("solved")
-    for ti in range(len(trucks)):
-        contained = [ boxes[bi] for bi in range(len(boxes)) if solver.Value(boxInTruck[(ti,bi)]) == 1]
-        print(f"truck {ti} is loaded with {contained}")
-else:
-    print("not solved")
+if status != cp_model.OPTIMAL and status != cp_model.FEASIBLE:
+    print("no solution found")
+    exit(-1)
+
+print("solution found")
+for ti in range(len(trucks)):
+    print(f"truck {trucks[ti]}:")
+    for bi in range(len(boxes)):
+        if solver.Value(boxInTruck[(bi,ti)]) == 1:
+            print(f"  {boxes[bi]}")
